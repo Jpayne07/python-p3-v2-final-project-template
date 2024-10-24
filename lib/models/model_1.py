71a9a015cacd8c4e050d8527b4acb3183638bd0a
Jacob <jacobpayne007@gmail.com>
2024-10-23 19:36:28 -0500

    added debgging connections

--- lib/models/model_1.py
+++ lib/models/model_1.py
@@ -2,6 +2,19 @@
 from __init__ import CURSOR, CONN
 
 #creating the first model class
-
 #one class
+class Job_board:
+    def __init__(self, job_title, company, location):
+        self.job_title = job_title
+        self.company = company
+        self.location = location
+
+#many class
+class Applicants:
+    all = [] #list of applicants
+    def __init__(self, name, job_id):
+        self.name = name
+        self.job_id = job_id
+
+
 
